31943c9535abc941f30e79c4b29022d7f11e5fcb
Sakarparaj <40866211+Sakarparaj@users.noreply.github.com>
2024-06-30 19:39:33 +0545

    Add files via upload
    All files added

---
+++ main.js
@@ -0,0 +1,120 @@
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+  document.body.style.visibility = 'visible';
+});
+
+const navOpen = () => {
+   navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+  document.body.style= 'visibility: visible; height: 100vh; width:100vw; overflow:hidden;';
+}
+
+const navClose = () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+  document.body.style= 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose)
+
+// AOS
+// AOS.refreshHard();
+AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+// const dishGridEl = Array.from(document.querySelectorAll('#dishGrid'));
+// if (dishGridEl.length > 0){
+//   // console.log(dishGridEl)
+//   dishGridEl.forEach(item => {
+//     item.setAttribute('data-aos', 'fade-up');
+//   })
+// }
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/**
+    Image slider
+ */
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+
+/**
+    MENU TAB
+ */
+
+
+
+function openMenu(evt, menuName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(menuName).style.display = "block";
+    if (evt) {
+        evt.currentTarget.classList.add("active");
+    }
+}
+
+// Default to displaying the first tab when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    openMenu(null, 'Breakfast');
+});
+
+
+
+
+
+
+
+  
